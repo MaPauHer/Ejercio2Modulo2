bd96cf4facdb6add12e38d42b82f8122479ab28c
MaPauHer <mpharg22@gmail.com>
2024-05-21 12:34:37 -0300

    Solución Ejercicio2Modulo2

---
+++ EjercicioClase2Modulo2/ClienteModificado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase2Modulo2
+{
+    internal class ClienteModificado
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int CodCliente { get; set; }
+        public string TiCliente { get; set; }
+    }
+}
--- EjercicioClase2Modulo2/Program.cs
+++ EjercicioClase2Modulo2/Program.cs
@@ -19,29 +19,65 @@
 
 
             #region Ejercicio1
-
             // Detectar cual es el numero mas grande e imprimirlo por consola
 
-            var lstNumeros = new List<int>() { 25,10,99,105, 1, 84, 22};
+            var lstNumeros = new List<int>() { 25, 10, 99, 105, 1, 84, 22 };
+
+            int maxEdad = lstNumeros.Max();
+
+            Console.WriteLine("\n\n Ejercicio 1  ");
+            Console.WriteLine("---------------------- ");
+            Console.WriteLine($"\n Numero maximo: {maxEdad}");
+
 
             #endregion
 
             #region Ejercicio2
-
             //Ordenar los nombres alfabeticamente
             var lstNombres = new List<string>() { "Ana", "Alejandro", "Alexis", "Pablo", "Carlos", "Anibal", "Carla", "Susana" };
 
+            var lstNomOrd = lstNombres.OrderBy(nombre => nombre);
+
+            Console.WriteLine("\n\n Ejercicio 2  ");
+            Console.WriteLine("---------------------- ");
+            Console.WriteLine("\n Lista nombres ordenada: ");
+
+            foreach (string nombre in lstNomOrd)
+            {
+                Console.WriteLine($" {nombre}");
+            }
 
             #endregion
 
             #region Ejercicio3
             // Utilizando la variable "lstClientes" filtrar los clientes que tengan vip = true e imprimirlo por consola
 
+
+            var lstCtesVip = lstClientes.Where(cliente => cliente.Vip);
+
+            Console.WriteLine("\n\n Ejercicio 3  ");
+            Console.WriteLine("---------------------- ");
+            Console.WriteLine("\n Lista clientes vip: ");
+
+            foreach (Clientes ClienteVip in lstCtesVip)
+            {
+                Console.WriteLine($" Apellido: {ClienteVip.Apellido}, Nombre: {ClienteVip.Nombre}, CodCte: {ClienteVip.CodCliente}, Vip: {ClienteVip.Vip}");
+            }
+
             #endregion
 
             #region Ejercicio4 
-
             //Utilizando la variable "lstClientes" crear una nueva lista donde solo se encuentren los nombres de los clientes e imprimir los nombres
+
+            Console.WriteLine("\n\n Ejercicio 4  ");
+            Console.WriteLine("---------------------- ");
+            Console.WriteLine("\n Lista nombre clientes: ");
+
+            lstClientes.ForEach(cliente =>
+            {
+                Console.WriteLine($" {cliente.Nombre}");
+
+            });
 
             #endregion
 
@@ -50,6 +86,83 @@
             // Nombre tiene que guardarse en mayusculas
             // Apellido tiene que guardarse en mayusculas
             // Vip => se debe evaluar el bool y si es true se debe asignar el texto "Premium" y si es false "Normal"
+
+            //**************************************************************************
+            // SOLUCION 1 - ojo que solo quedan los no VIP en la lista de modificados 
+            //**************************************************************************
+            //var lstCtesModif = lstClientes.Where(cliente => cliente.Vip == true)
+            //                    .Select(cliente => new ClienteModificado()
+            //                    {
+            //                        Nombre = cliente.Nombre.ToUpper(),
+            //                        Apellido = cliente.Apellido.ToUpper(),
+            //                        CodCliente = cliente.CodCliente,
+            //                        TiCliente = "Premium"
+            //                    })
+            //                    .ToList();
+
+            //lstCtesModif = lstClientes.Where(cliente => cliente.Vip == false)
+            //                .Select(cliente => new ClienteModificado()
+            //                {
+            //                    Nombre = cliente.Nombre.ToUpper(),
+            //                    Apellido = cliente.Apellido.ToUpper(),
+            //                    CodCliente = cliente.CodCliente,
+            //                    TiCliente = "Normal"
+            //                })
+            //                .ToList();
+
+
+            //**************************************************************************
+            // SOLUCION 2 - explicada por Ale 
+            //**************************************************************************
+            //var lstCtesModif = lstClientes.Select(cliente => new ClienteModificado()
+            //{
+            //    Nombre = cliente.Nombre.ToUpper(),
+            //    Apellido = cliente.Apellido.ToUpper(),
+            //    CodCliente = cliente.CodCliente,
+            //    TiCliente = cliente.Vip ? "Premium" : "Normal"
+            //})
+            //.ToList();
+
+            //**************************************************************************
+            // SOLUCION 3 - explicada por Ale :)
+            //**************************************************************************
+            var lstCtesModif = lstClientes.Select(cliente =>
+            {
+                string tiCte = "";
+                if (cliente.Vip)
+                {
+                    tiCte = "Premiun";
+                }
+                else
+                {
+                    tiCte = "Normal";
+                }
+                var CteModificado = new
+                {
+                    Nombre = cliente.Nombre.ToUpper(),
+                    Apellido = cliente.Apellido.ToUpper(),
+                    CodCliente = cliente.CodCliente,
+                    TiCliente = tiCte
+                };
+                return(CteModificado);
+            })
+            .ToList();
+
+
+            Console.WriteLine("\n\n Ejercicio 5  ");
+            Console.WriteLine("---------------------- ");
+            Console.WriteLine("\n Lista modificada: ");
+
+            lstCtesModif.ForEach(CteModif => Console.WriteLine($" Apellido: {CteModif.Apellido}, Nombre: {CteModif.Nombre}, CodCte: {CteModif.CodCliente}, TiCte: {CteModif.TiCliente}"));
+
+            //**************************************************************************
+            // IMPRIMIR SIN LINQ
+            //**************************************************************************
+            //foreach (var CteModif in lstCtesModif)
+            //{
+            //    Console.WriteLine($" Apellido: {CteModif.Apellido}, Nombre: {CteModif.Nombre}, CodCte: {CteModif.CodCliente}, TiCte: {CteModif.TiCliente}");
+            //}
+
             #endregion
         }
     }
